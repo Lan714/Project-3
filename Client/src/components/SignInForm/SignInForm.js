010e04d82e2930cbab8d9f6ac50a98b16ae786ef
Shannon Rayes <shannono18@gmail.com>
2021-10-19 11:16:54 -0400

    styling for registration

--- Client/src/components/SignInForm/SignInForm.js
+++ Client/src/components/SignInForm/SignInForm.js
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import UserAPI from '../../utils/UserAPI'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+
 
 const SignInForm = () => {
 	const [userState, setUserState] = useState({
@@ -48,7 +52,7 @@ const SignInForm = () => {
 					onChange={handleInputChange} />
 			</Form.Group>
 			<Button
-				variant="primary"
+				variant="dark"
 				type="submit"
 				onClick={handleLoginUser} >
 				Sign In
@@ -57,5 +61,34 @@ const SignInForm = () => {
 	)
 
 }
+const ExampleToast = ({ children }) => {
+	const [show, toggleShow] = useState(true);
+  
+	return (
+	  <>
+		{!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+		<Toast show={show} onClose={() => toggleShow(false)}>
+		  <Toast.Header>
+			<strong className="mr-auto">React-Bootstrap</strong>
+		  </Toast.Header>
+		  <Toast.Body>{children}</Toast.Body>
+		</Toast>
+	  </>
+	);
+  };
+  
+  const App = () => (
+	<Container className="p-3">
+	  <Jumbotron>
+		<h1 className="header">Welcome To React-Bootstrap</h1>
+		<ExampleToast>
+		  We now have Toasts
+		  <span role="img" aria-label="tada">
+			🎉
+		  </span>
+		</ExampleToast>
+	  </Jumbotron>
+	</Container>
+  );
 
 export default SignInForm
