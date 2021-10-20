f50525e985372761b5abd19e41bc4145baa1f5ea
jypkr <wodusqkr1210@gmail.com>
2021-10-20 13:19:08 -0700

    123123

--- Client/src/Pages/Home/home.js
+++ Client/src/Pages/Home/home.js
@@ -5,11 +5,17 @@ import Navbar from '../../components/Navbar'
 import './Home.css'
 
 const Home = () => {
+	const handleSignOut = () => {
+		localStorage.removeItem('token')
+		window.location = '/signin'
+	}
+
 	return (
 		<div className="homePg">
 			<Navbar />
 			<div className="pgContent">
 				Hello world
+				<Button onClick={handleSignOut}>Logout</Button>
 			</div>
 		</div>
 	)
--- Client/src/components/Navbar/navbar.js
+++ Client/src/components/Navbar/navbar.js
@@ -1,19 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleRight, faHome, faUsers, faTrophy, faSignOutAlt, faWallet } from '@fortawesome/free-solid-svg-icons'
-import { useHistory } from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-
   const history = useHistory()
-
-
-  const handleSignOut = () => {
-    localStorage.removeItem('token')
-    window.location = '/signin'
-  }
-
-
   return (
     <div className="sideNavbar">
 
@@ -22,13 +13,11 @@ const Navbar = () => {
         <li className="navTog">
           <section className="navLink">
             <span className="navSpan" >Project 3</span>
-            <FontAwesomeIcon icon={faAngleDoubleRight} style={{ color: "#3993CD" }} id="togIcon" />
+            <FontAwesomeIcon icon={faAngleDoubleRight} style={{color: "#3993CD"}} id="togIcon" />
           </section>
         </li>
 
         <li className="navTab">
-
-
           <div onClick={() => history.push('/')} className="navLink">
             <FontAwesomeIcon icon={faHome} className="icon" />
             <span className="navSpan">Home</span>
@@ -38,38 +27,26 @@ const Navbar = () => {
         <li className="navTab">
           <div onClick={() => history.push('/history')} className="navLink">
             <FontAwesomeIcon icon={faWallet} className="icon" />
-
-            <span className="navSpan">History</span>
-
+            <span className="navSpan">Transactions</span>
           </div>
         </li>
 
         <li className="navTab">
-
           <div onClick={() => history.push('/Leaderboard')} className="navLink">
-
-
-
             <FontAwesomeIcon icon={faTrophy} className="icon" />
             <span className="navSpan">Leaderboard</span>
           </div>
         </li>
 
         <li className="navTab">
-
           <div onClick={() => history.push('/Aboutus')} className="navLink">
-
-
-
             <FontAwesomeIcon icon={faUsers} className="icon" />
             <span className="navSpan">About Us</span>
           </div>
         </li>
 
         <li className="navTab">
-
-          <div onClick={handleSignOut} className="navLink">
-
+          <div onClick={() => history.push('/signIn')} className="navLink">
             <FontAwesomeIcon icon={faSignOutAlt} className="icon" />
             <span className="navSpan">Log out</span>
           </div>
