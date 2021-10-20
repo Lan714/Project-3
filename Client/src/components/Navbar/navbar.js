e2b5509427f35b7a7fb9a5ca264572a833d42b47
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-20 10:37:45 -0700

    Navbar changes

--- Client/src/components/Navbar/navbar.js
+++ Client/src/components/Navbar/navbar.js
@@ -4,16 +4,12 @@ import {useHistory} from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-<<<<<<< HEAD
-  const history = useHistory()
-=======
 
   const handleSignOut = () => {
     localStorage.removeItem('token')
     window.location = '/signin'
   }
 
->>>>>>> 1bdd4b396a7f291dbcacb4aba652e7cd879ca8ee
   return (
     <div className="sideNavbar">
 
@@ -27,11 +23,7 @@ const Navbar = () => {
         </li>
 
         <li className="navTab">
-<<<<<<< HEAD
-          <div onClick={() => history.push('/')} className="navLink">
-=======
           <a href="/" className="navLink">
->>>>>>> 1bdd4b396a7f291dbcacb4aba652e7cd879ca8ee
             <FontAwesomeIcon icon={faHome} className="icon" />
             <span className="navSpan">Home</span>
           </div>
@@ -45,33 +37,21 @@ const Navbar = () => {
         </li>
 
         <li className="navTab">
-<<<<<<< HEAD
-          <div onClick={() => history.push('/Leaderboard')} className="navLink">
-=======
           <a href="/leaderboard" className="navLink">
->>>>>>> 1bdd4b396a7f291dbcacb4aba652e7cd879ca8ee
             <FontAwesomeIcon icon={faTrophy} className="icon" />
             <span className="navSpan">Leaderboard</span>
           </div>
         </li>
 
         <li className="navTab">
-<<<<<<< HEAD
-          <div onClick={() => history.push('/Aboutus')} className="navLink">
-=======
           <a href="/aboutus" className="navLink">
->>>>>>> 1bdd4b396a7f291dbcacb4aba652e7cd879ca8ee
             <FontAwesomeIcon icon={faUsers} className="icon" />
             <span className="navSpan">About Us</span>
           </div>
         </li>
 
         <li className="navTab">
-<<<<<<< HEAD
-          <div onClick={() => history.push('/signIn')} className="navLink">
-=======
           <a onClick={handleSignOut} className="navLink">
->>>>>>> 1bdd4b396a7f291dbcacb4aba652e7cd879ca8ee
             <FontAwesomeIcon icon={faSignOutAlt} className="icon" />
             <span className="navSpan">Log out</span>
           </div>
